17d507243dd8df6f053a195890619f2058863935
TruongHuy <demonsjj.1999.dj@gmail.com>
2019-04-11 13:22:07 +0700

    Clearall

--- LayOut/LayOut/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LayOut.MvcApplication" Language="C#" %>
--- LayOut/LayOut/LayOut/LayOut/Scripts/cufon-replace.js
+++
@@ -1,1 +0,0 @@
-﻿Cufon.replace('#menu li a, #menu_close, #menu_down, h2', { fontFamily: 'Kozuka Gothic Pro OpenType', hover: true });
