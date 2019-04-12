459bb2dbdc1da537cfe6a5fff213321f89ddb09d
Le Khac Huy <lekh39@gmail.com>
2019-04-12 14:00:33 +0700

    All

---
+++ ClockUniverse/ClockUniverse/Controllers/product_typesController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ClockUniverse.Controllers
+{
+    public class product_typesController : Controller
+    {
+        //
+        // GET: /product_types/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
